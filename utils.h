213f6449c728bbe86eade1fae1b22d7fd6af4c0f
Nikolay Shalakin <nikolay.shalakin@astrosoft.ru>
2017-01-20 16:35:24 +0300

    move helper stuff to utils.{cpp,h}

---
+++ utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QFile>
+
+class QTableWidget;
+class QTableWidgetItem;
+
+bool isBinary(QFile &f);
+int callQuestionDialog(const QString &message);
+QString getFileNameDialog(const QString &title, const QString &message, const QString &text, QWidget *parent);
+void createFile(const QString &fileName);
+QPoint getFilePos(QTableWidget *w, const QString &str);
+void clearFilesList(QTableWidget *w);
+void resizeFilesColumns(QTableWidget *w);
+void boldenFileItem(QTableWidgetItem *item, bool bold);
+
+#endif // UTILS_H
